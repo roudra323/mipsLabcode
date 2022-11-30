63acadb33326e4f1f7c750aa8fa6ba4fb8e087d3
Farhan-FMS <61820314+fms-byte@users.noreply.github.com>
2022-12-01 02:35:19 +0600

    Update prime.asm
    There are some condition issues on your loop.

--- prime.asm
+++ prime.asm
@@ -1,51 +1,43 @@
-#problem 13: prime number
-
-
 .data 
-    userInput: .asciiz "give an integer to check if prime: "
-    prime: .asciiz "Prime"
-    notPrime: .asciiz "Not prime"
+    	userInput: .asciiz "Enter an integer to check if prime: "
+    	prime: .asciiz "Prime"
+    	notPrime: .asciiz "Not prime"
 .text
-    main:
+main:
         li $v0, 4
         la $a0, userInput
         syscall
     
-    li $v0, 5
-    syscall
-    move $t0, $v0
+    	li $v0, 5
+    	syscall
+    	move $t0, $v0
     
-    beq $t0,$zero, exit
-    beq $t0,2,yes
+    	beq $t0,$zero, Exit
+    	beq $t0,2,yes
     
-    li $t1,2 # divider
-    li $t2,0 # count
+    	li $t1,2 # divider
+    	li $t2,0 # count
     
-    loop: bgt $t1,$t0,exit
+    	loop:
+    		beq $t0,$t1,yes
+    		div $t0,$t1
+    		mfhi $s0
     
-    div $t0,$t1
-    mfhi $s0
+    		beqz $s0,no
+    		addi $t1,$t1,1
+    		j loop
+   	yes:
+   		li $v0,4
+   		la $a0,prime
+   		syscall
+   		j Exit 
     
-    beqz $s0,no
-    addi $t1,$t1,1
+	no:
+		li $v0,4
+   		la $a0,notPrime
+   		syscall
+   		j Exit
     
-    
-    
-    
-yes:
-    li $v0, 4
-    la $a0, prime
-    syscall
-    j exit
-    
-no:
-    li $v0, 4
-    la $a0, notPrime
-    syscall
-    j exit
-    
-    
-    
-exit:
-    li $v0, 10
-    syscall
+	Exit:	
+    		li $v0, 10
+    		syscall
